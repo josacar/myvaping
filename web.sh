2df7c98f76faa47b737aa7be77e13aff5c24a673
Jose Luis Salas <1630686+josacar@users.noreply.github.com>
2024-07-13 10:02:38 +0200

    add custom layout

--- web.sh
+++ web.sh
@@ -1,3 +1,3 @@
 export VODKA_HOME=config/vodka
-poetry run gunicorn --log-level debug --bind 0.0.0.0:7021 vodka.runners.wsgi:application 
+poetry run gunicorn --log-level info --bind 0.0.0.0:7021 vodka.runners.wsgi:application 
 # poetry run -H venv/ --socket=0.0.0.0:7026 -w vodka.runners.wsgi:application --enable-threads
