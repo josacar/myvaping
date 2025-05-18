56c690880937c51c2ab080467a453b29b17545ba
Jose Luis Salas <josacar@users.noreply.github.com>
2025-05-18 12:40:04 +0200

    Add zeromq for web

--- web.sh
+++ web.sh
@@ -1,3 +1,3 @@
 export VODKA_HOME=config/vodka
-uv run --extra standalone --extra gunicorn --extra whisper gunicorn --log-level info --bind 0.0.0.0:7021 vodka.runners.wsgi:application
+uv run --extra standalone --extra gunicorn --extra whisper --extra zeromq gunicorn --log-level info --bind 0.0.0.0:7021 vodka.runners.wsgi:application
 # uv run -H venv/ --socket=0.0.0.0:7026 -w vodka.runners.wsgi:application --enable-threads
